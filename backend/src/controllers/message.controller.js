249f32be9887a954e6d1903c9ea6b7f21058699a
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-05-14 22:08:21 +0530

    message

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -16,12 +16,49 @@ export const getUsersForSidebar = async(req,res) => {
 export const getMessages = async(req, res) => {
     try {
         const { id:userToChatId } = req.params
-        const senderId = req.user._id;
+        const myId = req.user._id;
 
         const messages = await Message.find({
-            
-        })
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId }
+            ],
+        });
+
+        res.status(200).json(messages);
     } catch (error) {
-        
+        console.log("Error in getMessages controller: ", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const sendMessage = async(req, res) => {
+    try {
+        const { text, image } =  req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            //upload image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        // todo: realtime functionality goes here => socket.io
+
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.log("Error in sendMessage controller: ", error.message);
+        res.status(500).json({ error: "Internal server error " });
     }
 }
