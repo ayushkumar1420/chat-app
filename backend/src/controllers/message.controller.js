404fa144075de8eb270503445db5ba0832507329
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-05-14 21:48:42 +0530

    export

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js"
+import Message from "../models/message.model.js"
 
 export const getUsersForSidebar = async(req,res) => {
     try {
@@ -11,3 +12,16 @@ export const getUsersForSidebar = async(req,res) => {
       res.status(500).json({ error: "Internal server error" });
     }
 };
+
+export const getMessages = async(req, res) => {
+    try {
+        const { id:userToChatId } = req.params
+        const senderId = req.user._id;
+
+        const messages = await Message.find({
+            
+        })
+    } catch (error) {
+        
+    }
+}
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -5,5 +5,6 @@ import { getUsersForSidebar } from "../controllers/message.controller.js"
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar)
+router.get("/:id", protectRoute, getMessages)
 
 export default router;
