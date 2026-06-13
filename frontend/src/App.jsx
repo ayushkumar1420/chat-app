f7a7d9f664cd6e732938993301dc61f6f4a17093
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-06-13 21:32:16 +0530

    loader added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,15 +7,23 @@ import SettingPage from "./pages/SettingPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 const App = () => {
-  const { authUser, checkAuth } = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
 
   console.log({ authUser });
+
+  if (isCheckingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />    
+      </div>
+    )
   
   return (
     <div>
