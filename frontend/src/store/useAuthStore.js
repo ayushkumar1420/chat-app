4ed04d12d5779ebd859c8fdc20bb07ac20924e05
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-06-25 22:32:08 +0530

    logout function

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -49,4 +49,14 @@ export const useAuthStore = create((set) => ({
             set({ isSigningUp: false });
         }
     },
+
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            set({ authUser: null });
+            toast.success("logout out successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    }
 }));
