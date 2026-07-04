9ba051a33f69103f0fadebdc0304d3306ab58760
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-07-04 23:01:24 +0530

    resolving issue in json files

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,15 @@
 import { create } from "zustand";
 
-export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("chat-theme") || "coffee",
-  setTheme: (theme) => {
-    localStorage.setItem("chat-theme", theme);
-    set({ theme });
-  },
-}));     
+export const useThemeStore = create((set) => {
+  const initialTheme = localStorage.getItem("chat-theme") || "coffee";
+  document.documentElement.setAttribute("data-theme", initialTheme);
+  
+  return {
+    theme: initialTheme,
+    setTheme: (theme) => {
+      localStorage.setItem("chat-theme", theme);
+      document.documentElement.setAttribute("data-theme", theme);
+      set({ theme });
+    },
+  };
+});     
