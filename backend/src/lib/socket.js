964abf5fc5e62a03ab8b78ec8705ae68968c3cba
ayushkumar1420 <ayushmaurya496@gmail.com>
2026-08-06 21:20:52 +0530

    jwt cookie error that shows err 404

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,10 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://chat-app-kappa-seven-42.vercel.app",
-    ],
+    origin: process.env.CLIENT_URL,
     credentials: true,
   },
 });
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,17 +1,22 @@
-import jsonwebtoken from "jsonwebtoken"
+import jsonwebtoken from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-    const isProduction = process.env.NODE_ENV === "production";
-    const token = jsonwebtoken.sign({userId}, process.env.JWT_SECRET, {
-        expiresIn:"7d"
-    })
+  const isProduction = process.env.NODE_ENV === "production";
 
-    res.cookie("jwt",token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true,
-        sameSite: isProduction ? "strict" : "lax",
-        secure: isProduction
-    })
+  const token = jsonwebtoken.sign(
+    { userId },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    }
+  );
 
-    return token;
+  res.cookie("jwt", token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+  });
+
+  return token;
 };
